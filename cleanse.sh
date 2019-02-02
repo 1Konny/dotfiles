7c216f7a16e636993140dc8c141265123e75d0ef
1Konny <lee.wonkwang94@gmail.com>
2019-02-02 16:22:01 +0900

    add cleanser

---
+++ cleanse.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+rm -rf $HOME/.oh-my-zsh
+rm -rf $HOME/.aliases
+rm -rf $HOME/.tmux.conf
+rm -rf $HOME/.gitconfig
+rm -rf $HOME/.Xmodmap
+rm -rf $HOME/.vimrc
+rm -rf $HOME/.vim
+rm -rf $HOME/.zshrc
+rm -rf $HOME/.gitconfig.secret
