844249200c5390068b69f6a847fc3af59074d839
1Konny <lee.wonkwang94@gmail.com>
2019-02-02 16:20:04 +0900

    refactor

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
-# Argument Parser
-# USAGE : https://stackoverflow.com/questions/7069682/how-to-get-arguments-with-flags-in-bash
-while getopts 'c' flag; do
+#==================================================#
+# argument parser
+# usage : https://stackoverflow.com/questions/7069682/how-to-get-arguments-with-flags-in-bash
+while getopts 'cfu:' flag; do
   case "${flag}" in
     c) install_conda='true' ;;
+    f) forced='true' ;;
+    u) update="${OPTARG}" ;;
   esac
 done
 
@@ -16,41 +19,26 @@ ZSH_CUSTOM=$ZSH/custom
 
 
 #==================================================#
-# set zsh to the default shell
-echo 'Set ZSH as default shell'
-chsh -s $(which zsh)
-
-
-#==================================================#
-# function to backup existing dotfiles. 
-# (buo : back-up-original)
-function buo(){
-    backup_dir="$HOME/dotfiles_backup"
-    name=$1
-    path="$HOME/$name"
-
-    if [ -f $path ] || [ -h $path ] || [ -d $path ];
-    then
-        mkdir -p $backup_dir
-        new_path="$backup_dir/$name"
-        printf "Found '$path'. Backing up to '$new_path\n"
-        mv $path $new_path
-    fi
-}
-
-buo .Xmodmap; ln -sf $DF_DIR/Xmodmap $HOME/.Xmodmap 
-buo .vim; buo .vimrc; ln -sf $DF_DIR/vimrc $HOME/.vimrc
-buo .tmux.conf; ln -sf $DF_DIR/tmux.conf $HOME/.tmux.conf
-buo .aliases; ln -sf $DF_DIR/aliases $HOME/.aliases
-buo .gitconfig; ln -sf $DF_DIR/gitconfig $HOME/.gitconfig
-buo .zshrc; ln -sf $DF_DIR/zshrc $HOME/.zshrc
-buo .oh-my-zsh; bash $DF_DIR/install-omz.sh; ln -sf $DF_DIR/themes/mrtazz_custom.zsh-theme $HOME/.oh-my-zsh/themes/
+source "$PWD/aliases/misc"
+if [ "$forced" != "true" ]; then
+    buo .Xmodmap .vim .vimrc .tmux.conf .aliases .gitconfig .gitconfig.secret .zshrc .oh-my-zsh 
+fi
+ln -sf $DF_DIR/Xmodmap $HOME/.Xmodmap 
+ln -sf $DF_DIR/vimrc $HOME/.vimrc
+ln -sf $DF_DIR/tmux.conf $HOME/.tmux.conf
+ln -sf $DF_DIR/aliases $HOME/.aliases
+ln -sf $DF_DIR/gitconfig $HOME/.gitconfig
+ln -sf $DF_DIR/zshrc $HOME/.zshrc
+echo; echo '** download oh-my-zsh.'
+bash $DF_DIR/install-omz.sh; 
+ln -sf $DF_DIR/themes/mrtazz_custom.zsh-theme $HOME/.oh-my-zsh/themes/
 
 
 #==================================================#
 # download useful plugins
 
 # zsh
+echo; echo '** download zsh plugins.'
 # alias-tip 
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-$ZSH/custom}/plugins/alias-tips
 # zsh-syntax-highlighting 
@@ -59,6 +47,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$ZSH/custom}/plugins/zsh-autosuggestions
 
 # vim 
+echo; echo '** download vim plugins.'
 # colorschemes
 mkdir $HOME/.vim
 git clone https://github.com/flazz/vim-colorschemes.git $HOME/.vim
@@ -75,15 +64,24 @@ git clone https://github.com/vim-airline/vim-airline-themes $HOME/.vim/bundle/vi
 
 #==================================================#
 # anaconda3
-if [ "$install_conda" == "true" ]; 
+if [ "$install_conda" = "true" ]; 
 then
-    wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh -O anaconda.sh
-    bash anaconda.sh -b -p $HOME/anaconda3
-    rm anaconda.sh
-    conda update conda --yes
-    conda update anaconda --yes
+    source "$PWD/aliases/conda"
+    if [ $(checkconda) = "true" ]; then
+        echo; echo "** anaconda already exists."
+    else
+        echo; echo "** anaconda does not exist. download and install anaconda3."
+        wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh -O anaconda.sh
+        bash anaconda.sh -b -p $HOME/anaconda3
+        rm anaconda.sh
+        #conda update conda --yes
+        #conda update anaconda --yes
+    fi
 fi
 
 
 #==================================================#
+# set zsh to the default shell
+echo; echo '** set ZSH as default shell.'
+echo "exec zsh" >> $HOME/.bash_profile
 exec zsh
