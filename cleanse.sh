b7d345cae14793a6868c9213cc0e338056403f10
1Konny <lee.wonkwan94@gmail.com>
2019-02-03 12:33:40 +0900

    .

--- cleanse.sh
+++ cleanse.sh
@@ -4,6 +4,8 @@ rm -rf $HOME/.oh-my-zsh
 rm -rf $HOME/.aliases
 rm -rf $HOME/.tmux.conf
 rm -rf $HOME/.gitconfig
+rm -rf $HOME/.gitconfig.secret
+rm -rf $HOME/.condarc
 rm -rf $HOME/.Xmodmap
 rm -rf $HOME/.vimrc
 rm -rf $HOME/.vim
